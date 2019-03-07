7b37a8d683d999886c8927de07d32c6fcc17a41c
Apl-Serova <47252760+Apl-Serova@users.noreply.github.com>
2019-03-07 21:47:53 +0300

    Update Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -1,17 +1,9 @@
-
 import java.util.Scanner;
-public class Serova {
+public class Exercise5 {
     public static void main(String [] args) {
-        Scanner s = new Scanner(System.in);
-        int x = s.nextInt();
-        System.out.println(x+"*1="+x);
-        System.out.println(x+"*2="+x*2);
-        System.out.println(x+"*3="+x*3);
-        System.out.println(x+"*4="+x*4);
-        System.out.println(x+"*5="+x*5);
-        System.out.println(x+"*6="+x*6);
-        System.out.println(x+"*7="+x*7);
-        System.out.println(x+"*8="+x*8);
-        System.out.println(x+"*9="+x*9);
+        System.out.println("Введите бинарный код: ");
+        Scanner str = new Scanner(System.in);
+        String s=str.nextLine();
+        System.out.println("Бинарное число "+s+" в десятичной записи: "+ Integer.valueOf(s,2));
     }
 }
