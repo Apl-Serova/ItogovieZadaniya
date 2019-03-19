e047605dbc83e0394189256071f6e22776517b86
Apl-Serova <47252760+Apl-Serova@users.noreply.github.com>
2019-03-19 20:27:25 +0300

    Update Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -3,7 +3,21 @@ public class Exercise5 {
     public static void main(String [] args) {
         System.out.println("Введите бинарный код: ");
         Scanner str = new Scanner(System.in);
-        String s=str.nextLine();
-        System.out.println("Бинарное число "+s+" в десятичной записи: "+ Integer.valueOf(s,2));
+        int n =str.nextInt();
+        int a = 0;
+        int b = 0;
+        int c = n;
+        for (int i = 10; i != 0; i *= 10 ) {
+            if ( (n % i) / (i / 10) == 1) {
+                a += Math.pow(2, b);
+            }
+            c /= 10;
+            if (c == 0) {
+                i = 0;
+            }
+            b += 1;
+        }
+        System.out.println("Число в 10 с.с. : " + a);
+
     }
 }
