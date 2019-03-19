28eba65bab459c4f480f747c48071bcf0c5f3c3f
Apl-Serova <47252760+Apl-Serova@users.noreply.github.com>
2019-03-19 19:55:03 +0300

    Update Ex2.java

--- Ex2.java
+++ Ex2.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 public class Exercise14 {
     public static void main (String [] args) {
-        System.out.println("Введите 5 элементов массива: ");
-        int[] chis = new int[5];
+        System.out.println("Введите количество элементов массива: ");
         Scanner s = new Scanner(System.in);
-        for (int i = 0; i<5; i++) {
+        int k = s.nextInt();
+        System.out.println("Введите " + k + " элементов массива: ");
+        int[] chis = new int[k];
+
+        for (int i = 0; i<k; i++) {
             chis[i] = s.nextInt();
         }
-        for (int i = 0; i<5; i++) {
-            for (int j = 0; j < 4; j++) {
+        for (int i = 0; i<k; i++) {
+            for (int j = 0; j < (k-1); j++) {
                 if (chis[j] > chis[j + 1]) {
                     int b = chis[j];
                     int m = chis[j + 1];
@@ -17,7 +20,9 @@ public class Exercise14 {
                 }
             }
         }
-        System.out.println(chis[0]+" "+ chis[1]+" "+chis[2]+" "+chis[3]+" "+chis[4]);
+        for (int i = 0; i<k; i++) {
+            System.out.println(chis[i]);
+        }
 
     }
 }
